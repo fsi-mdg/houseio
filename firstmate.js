ce3357717c111eaa21a831b841f850cd4990179c
fsi-mdg <mitchgram@gmail.com>
2019-12-08 13:43:52 -0600

    fist version that mult web access works!

--- firstmate.js
+++ firstmate.js
@@ -5,9 +5,16 @@ var io = require('socket.io');
 var os = require('os');
 var sensorLib = require("node-dht-sensor");
 var sensorResult = sensorLib.read(22, 12);
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var Zone1 = new Gpio(4, 'out'); //use GPIO pin 4 as output
+var Zone2 = new Gpio(17, 'out'); //use GPIO pin 17 as output
+var Zone3 = new Gpio(27, 'out'); //use GPIO pin 27 as output
+var Zone4 = new Gpio(22, 'out'); //use GPIO pin 22 as output
 
 // Output OS Stats
 pistats();
+// Initialize GPIO relays to off
+init_GPIO(0);
 
 console.log("Starting Web Server");
 var server = http.createServer(function(request, response){
@@ -16,7 +23,7 @@ var server = http.createServer(function(request, response){
     switch(path){
         case '/':
             response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write('this is the root director of the server');
+            response.write('Enter "/appname.html" to Enter Function on this server');
             response.end();
             break;
         case '/firstmate.html':
@@ -43,65 +50,110 @@ var server = http.createServer(function(request, response){
 
 server.listen(8001);
 console.log("Listening on 8001");
+
 io.listen(server);
 console.log("socket.io started");
 
 var listener = io.listen(server);
 
 // socket.io 
+
 listener.sockets.on('connection', function(socket){
-    //send data to client
-    setInterval(function(){
-
-        var sensorResult = sensorLib.read(22, 12);
-        var ftemp = sensorResult.temperature*9/5+32;
-        var humidity = sensorResult.humidity
-
-        socket.emit('temp', {'temp': ftemp.toFixed(2)});
-        socket.emit('humidity', {'humidity': humidity.toFixed(2)});
-        socket.emit('date', {'date': new Date()});
-
-//        console.log("Date: "+new Date()+" Temp:"+ftemp.toFixed(2)+" Humidity:"+humidity.toFixed(2));
-    }, 5000);
-    //recieve client data
-    socket.on('client_data', function(stuff){
-      process.stdout.write(stuff.letter);
-     //debug
-//      console.log('obj', stuff);
-//      console.log('.toString', stuff.toString());
-//      console.log('String()', String(stuff));
-//      console.log('JSON.stringify',JSON.stringify(stuff));
-
-    });
-
-});
+//send data to client
+  console.log('New client has connected'+socket);
+  // Read the current Zone Switch Values from Hardware 
+  var boxvalue = Zone1.readSync()
+  socket.emit('Zone1init', {'Zone1init': boxvalue});
+  console.log('Zone1 Init Status: '+ boxvalue);
+  
+  var boxvalue = Zone2.readSync();
+  socket.emit('Zone2init', {'Zone2init': boxvalue});
+  console.log('Zone2 Init Status: '+ boxvalue);
+  
+  var boxvalue = Zone3.readSync();
+  socket.emit('Zone3init', {'Zone3init': Zone3.boxvalue});
+  console.log('Zone3 Init Status: '+ boxvalue);
+  
+  var boxvalue = Zone4.readSync();
+  socket.emit('Zone4init', {'Zone4init': boxvalue});
+  console.log('Zone4 Iniit Status: '+ boxvalue);
 
 
-function pistats(){
-console.log('os.tmpdir:'+os.tmpdir());
-console.log('os.endianness:'+os.endianness());
-console.log('os.hostname:'+os.hostname());
-console.log('os.type:'+os.type());
-console.log('os.platform:'+os.platform());
-console.log('os.arch:'+os.arch());
-console.log('os.uptime:'+formatSeconds(os.uptime()));
-console.log('os.loadavg:'+os.loadavg());
-console.log('os.totalmem (MB):'+os.totalmem()/1000000);
-console.log('os.freemem (MB):'+os.freemem()/1000000);
-var cpus = os.cpus();
-console.log('# of CPUs:'+cpus.length);
-for (var i=0; i< cpus.length; i++){
-    console.log('   CPU #'+i+' / '+cpus[i].model);
+  setInterval(function(){
+    var sensorResult = sensorLib.read(22, 12);
+    var ftemp = sensorResult.temperature*9/5+32;
+    var humidity = sensorResult.humidity
+    socket.emit('temp', {'temp': ftemp.toFixed(2)});
+    socket.emit('humidity', {'humidity': humidity.toFixed(2)});
+    socket.emit('date', {'date': new Date()});
+    var boxvalue = Zone1.readSync()
+    socket.emit('Zone1init', {'Zone1init': boxvalue});
+   },5000);
+
+
+  //recieve client data
+  socket.on('client_data', function(stuff){
+  process.stdout.write(stuff.letter);
+          console.log('obj', stuff);
+          console.log('.toString', stuff.toString());
+          console.log('String()', String(stuff));
+          console.log('JSON.stringify',JSON.stringify(stuff));
+  });
+  socket.on('Zone1', function(data) { //get light switch status from client
+    boxvalue = data;
+    console.log(new Date()+": " + boxvalue + "-Zone1");
+    if (boxvalue != Zone1.readSync()) { //only change LED if status has changed
+      Zone1.writeSync(boxvalue); //turn LED on or off
     }
-console.log('os.networkInterfaces:');
-var ifaces = os.networkInterfaces();
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-  ifaces[ifname].forEach(function (iface) {
-      console.log("  "+ifname, iface.address);
-    ++alias;
+  });
+  socket.on('Zone2', function(data) { //get light switch status from client
+    boxvalue = data;
+    console.log(new Date()+": " + boxvalue + "-Zone2");
+    if (boxvalue != Zone2.readSync()) { //only change LED if status has changed
+      Zone2.writeSync(boxvalue); //turn LED on or off
+    }
+  });
+  socket.on('Zone3', function(data) { //get light switch status from client
+    boxvalue = data;
+    console.log(new Date()+": " + boxvalue + "-Zone3");
+    if (boxvalue != Zone3.readSync()) { //only change LED if status has changed
+      Zone3.writeSync(boxvalue); //turn LED on or off
+    }
+  });
+  socket.on('Zone4', function(data) { //get light switch status from client
+    boxvalue = data;
+    console.log(new Date()+": " + boxvalue + "-Zone4");
+    if (boxvalue != Zone4.readSync()) { //only change LED if status has changed
+      Zone4.writeSync(boxvalue); //turn LED on or off
+    }
   });
 });
+
+function pistats(){
+  console.log('os.tmpdir:'+os.tmpdir());
+  console.log('os.endianness:'+os.endianness());
+  console.log('os.hostname:'+os.hostname());
+  console.log('os.type:'+os.type());
+  console.log('os.platform:'+os.platform());
+  console.log('os.arch:'+os.arch());
+  console.log('os.uptime:'+formatSeconds(os.uptime()));
+  console.log('os.loadavg:'+os.loadavg());
+  console.log('os.totalmem (MB):'+os.totalmem()/1000000);
+  console.log('os.freemem (MB):'+os.freemem()/1000000);
+  var cpus = os.cpus();
+  console.log('# of CPUs:'+cpus.length);
+  for (var i=0; i< cpus.length; i++){
+    console.log('   CPU #'+i+' / '+cpus[i].model);
+  }
+  console.log('os.networkInterfaces:');
+  var ifaces = os.networkInterfaces();
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+    ifaces[ifname].forEach(function (iface) {
+      console.log("  "+ifname, iface.address);
+    ++alias;
+    });
+  });
 }
 
 function formatSeconds(sec) {
@@ -109,4 +161,22 @@ function formatSeconds(sec) {
             .map(v => v < 10 ? "0" + parseInt(v) : parseInt(v))
             .filter((i, j) => i !== "00" || j > 0)
             .join(":");
+}
+
+
+function init_GPIO(init){
+  var initval = init;
+  Zone1.writeSync(initval);
+  initval=Zone1.readSync();
+  console.log("Zone1 Init: "+initval);
+  Zone2.writeSync(initval);
+  initval=Zone2.readSync();
+  console.log("Zone2 Init: "+initval);
+  Zone3.writeSync(initval);
+  initval=Zone3.readSync();
+  console.log("Zone3 Init: "+initval);
+  Zone4.writeSync(initval);
+  initval=Zone4.readSync();
+  console.log("Zone4 Init: "+initval);
+  console.log("---------------------");
 }
